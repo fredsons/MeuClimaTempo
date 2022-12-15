dfeaefb8a77eab6f74140a52b4c2422ac887ca3b
fsousaentre <85347233+fredsons@users.noreply.github.com>
2022-12-15 18:53:11 -0300

    primeira configuração

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Variaveis e seleção de elementos
+const apiKey = "761d1de52ba55a991db06a4a7dddf9b2";
+const apiCountryURL = "http://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Funções
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("teste");
+});
